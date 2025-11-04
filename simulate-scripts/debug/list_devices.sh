574cefe6e9b72046b82ac23e127dd2c83c38bba9
Newbie <adsl53102@gmail.com>
2025-11-04 22:54:47 +0800

    Refactor the project structure

--- simulate-scripts/debug/list_devices.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source ../config.sh
-
-curl -s -X GET \
-	   "http://${IOTA_HOST}:${IOTA_NORTH_PORT}/iot/devices" \
-	     -H "Fiware-Service: ${FIWARE_SERVICE}" \
-	       -H "Fiware-Servicepath: ${FIWARE_SERVICEPATH}" \
-	         | jq .
